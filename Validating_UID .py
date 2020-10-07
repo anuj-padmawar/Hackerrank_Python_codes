a270753154443ab17eb75c8c3e17a40c52046a48
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-10-07 12:02:57 +0530

    Add files via upload

--- Validating_UID .py
+++ Validating_UID .py
@@ -1,42 +1,15 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
 
-def uid_validator(uid):
-    characters = list(uid)
-    if len(uid) != 10:
-        return False
-
+for i in range(int(input())):
+    x=''.join(sorted(input()))
     try:
-        first_char=int(characters[0])
-    except ValueError:
-        pass
+        assert re.search(r'[A-Z]{2}',x)
+        assert re.search(r'\d\d\d',x)
+        assert not re.search(r'[^a-zA-Z0-9]',x)
+        assert not re.search(r'(.)\1',x)
+        assert len(x)==10
+    except:
+        print('Invalid')
     else:
-        return False
+        print('Valid')
 
-    digit_count=0
-    uppercase_count=0
-    for char in characters:
-        try:
-            if characters.count(char)>1:
-                return False
-            if int(char) in range(10):
-                digit_count+=1
-        except ValueError:
-            if char.isupper():
-                uppercase_count += 1
-            continue
-
-    if digit_count < 3 and uppercase_count < 3:
-        return False
-
-    return True
-
-
-if __name__ == "__main__":
-
-    input_no = int(input())
-    for i in range(input_no):
-        uid = input()
-        if uid_validator(uid):
-            print("Valid")
-        else:
-            print("Invalid")
